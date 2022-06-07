ab232005066b0b6de14bd71a37d920ff09bd2f8f
hle0 <91701075+hle0@users.noreply.github.com>
2022-06-07 18:05:45 -0400

    add support for picking audio files and playing them back!

--- js/fft/FFTdata.js
+++ js/fft/FFTdata.js
@@ -6,17 +6,24 @@ class _spectrogram {
     this.analyser = false;
   }
   init(stream) {
-    this.audioCtx = new AudioContext();
-    this.analyser = this.audioCtx.createAnalyser();
-    this.analyser.fftSize = 2048*4;
-    this.tmpStream = this.audioCtx.createMediaStreamSource(stream);
-    this.audioCtx.createMediaStreamSource(stream).connect(this.analyser);
-    this.analyser.smoothingTimeConstant = 0;
+    this.resetAnalyser(stream);
 
     this.data = new Uint8Array(this.analyser.frequencyBinCount);
 
     this.analyserFrequencyBinCount = this.analyser.frequencyBinCount;
     this.analyserSampleRate = this.audioCtx.sampleRate;
+  }
+  resetAnalyser(stream) {
+    if (this.audioCtx) this.audioCtx.close();
+    this.audioCtx = new AudioContext();
+    this.resetContext();
+    this.audioCtx.createMediaStreamSource(stream).connect(this.analyser);
+    this.tmpStream = this.audioCtx.createMediaStreamSource(stream);
+  }
+  resetContext() {
+    this.analyser = this.audioCtx.createAnalyser();
+    this.analyser.fftSize = 2048*4;
+    this.analyser.smoothingTimeConstant = 0;
   }
   update() {
   this.analyser.getByteFrequencyData(this.data);
@@ -26,5 +33,16 @@ class _spectrogram {
     // this.empty = new Uint8Array(this.data.length);
     // transform(this.data, this.empty);
     // console.log(this.empty);
+  }
+  changeStream(stream) {
+    this.audioCtx.close();
+    this.audioCtx = null;
+    this.resetAnalyser(stream);
+  }
+  swapContext(ctx) {
+    let ret = this.audioCtx;
+    this.audioCtx = ctx;
+    this.resetContext();
+    return ret;
   }
 }
